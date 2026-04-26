a2864a3c310e9d95b1f165bc0a06785b917e95b4
Nidhi Maurya <mauryanidhi1610@gmail.com>
2026-04-26 21:34:11 +0530

    learn state variable in class function

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,19 @@
-
+import { useState } from "react";
 export default function User(){
+
+
+const [count,setCount]=useState(0);
+const [count2,setCount2]=useState(5); 
+
+
+
 
   return (
     <>
      <div className="p-10 border m-5">
+      <h1>count={count}</h1>
+      <h1>Count2={count2}</h1>
+       <button className="p-1 rounded-md  border " onClick={()=>setCount(count+1)}> Increase</button>
        <h2>Name: Nidhi </h2>
        <h3>Location: Delhi</h3>
        <h4>Contact: 1234567890</h4>
--- src/components/UserClass.jsx
+++ src/components/UserClass.jsx
@@ -6,17 +6,27 @@ class UserClass extends React.Component{
 
     constructor(props){
       super(props);
+       this.state={
+        count:0, 
+        count2:5,
     }
 
-
+  };
   render(){
+
+const {name, location,contact,email}= this.props;
+const {count,count2}=this.state
+
+
     return (
       <>
       <div className="p-10 border m-5">
-       <h2> name: {this.props.name} </h2>
-       <h3>{this.props.location}</h3>
-       <h4>{this.props.contact}</h4>
-        <h4>{this.props.email}</h4>
+        <h1>Count: {count}</h1>
+        <h1> Count: {count2}</h1>
+       <h2> name: {name} </h2>
+       <h3>loaction:{location}</h3>
+       <h4>contact:{contact}</h4>
+        <h4>email:{email}</h4>
 
      </div>
     
